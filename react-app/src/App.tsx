c0a3285c83fb73187330dc6e2894148b043b127e
Lucasz-27 <escobarlucas27.10@gmail.com>
2025-09-07 17:58:34 -0300

    update 07/09

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,3 +1,48 @@
+/*
+import LetterGlitch from './components/LetterGlitch';
+import Card, { CardBody } from './components/Card';
+import List from './components/List';
+
+function App() {
+  const list = ['goku', 'vegeta', 'bulma'];
+
+  return (
+    <>
+      <Card>
+        <CardBody title='Hola Mundo' text='Este es el texto' />
+        <List data={list} />
+      </Card>
+      <LetterGlitch
+        glitchSpeed={50}
+        centerVignette={true}
+        outerVignette={false}
+        smooth={true}
+      />
+    </>
+  );
+}
+
+export default App;
+*/
+/*import Lightning from './components/Lightning';
+
+function App() {
+  return (<div style={{ width: '100%', height: '600px', position: 'relative' }}>
+    <Lightning
+      hue={220}
+      xOffset={0}
+      speed={1}
+      intensity={1}
+      size={1}
+    />
+  </div>
+  )
+}
+
+export default App
+*/
+
+
 import LiquidEtherBackground from './components/LiquidEtherBackground';
 
 function App() {
@@ -13,7 +58,8 @@ function App() {
 
 export default App;
 
-/*import Card, { CardBody } from './components/Card'
+/*
+import Card, { CardBody } from './components/Card'
 import List from './components/List'
 
 
