178e32705a25de9a21bf714e70ff7bac31788a3f
Lucasz-27 <escobarlucas27.10@gmail.com>
2025-09-07 02:06:00 -0300

    first commit

---
+++ react-app/src/App.tsx
@@ -0,0 +1,14 @@
+import Card, { CardBody } from './components/Card'
+import List from './components/List'
+
+
+function App() {
+  const list = ['goku', 'vegeta', 'bulma']
+  return <Card>
+    <CardBody title='Hola Mundo' text='Este es el texto' />
+    <List data={list} />
+  </Card>
+}
+
+export default App
+
---
+++ react-app/src/titulo.tsx
@@ -0,0 +1,5 @@
+function titulo() {
+    return <h1>Hello World :3</h1>
+}
+
+export default titulo
